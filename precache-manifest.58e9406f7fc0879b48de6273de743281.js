f2d96bad8206eea5e5ce5bbce7eb410968f21d04
Karol Makara <karol_makara17@wp.pl>
2019-05-23 19:14:00 +0200

    Updates

---
+++ precache-manifest.58e9406f7fc0879b48de6273de743281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bf8f422b61ba4c162487a2c297c308",
+    "url": "/PollutionApp/index.html"
+  },
+  {
+    "revision": "e2d8df8858f11f7d53ac",
+    "url": "/PollutionApp/static/css/main.60e7ca09.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/PollutionApp/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "e2d8df8858f11f7d53ac",
+    "url": "/PollutionApp/static/js/main.601d7b98.chunk.js"
+  },
+  {
+    "revision": "58109a6bd202194ece7f",
+    "url": "/PollutionApp/static/js/runtime~main.4b40fbec.js"
+  }
+]);
