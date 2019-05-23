9ddcf6214a85490c75dac0e6dc9cdff83f29e8c9
Karol Makara <karol_makara17@wp.pl>
2019-05-23 19:57:30 +0200

    remove variables and returns

--- src/components/City.js
+++ src/components/City.js
@@ -7,31 +7,28 @@ class City extends Component {
     }
     
     componentDidMount(){
-        const API = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&indexpageids&titles=";
-        const city = this.props.name;
-        
-        fetch(API + city)
+        const API = 'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&indexpageids&titles=';
+        fetch(API + this.props.name)
             .then(response => response.json())
             .catch(error => console.log('Error:', error))
             .then(data => {
-                const pageids = data.query.pageids;
+                const pageIds = data.query.pageids;
                 const pages = data.query.pages;
-                return this.setState({description: pages[pageids].extract })
-            })
-            
-            
+                this.setState({description: pages[pageIds].extract })
+            })     
     }
 
     render() {
         const {name, id} = this.props;
         return (
+            //Bootstrap to remove
             <div className="card">
                 <div className="card-header" id={`heading${id}`}>
-                <h5 className="mb-0">
+                <h4 className="mb-0">
                     <button className="btn btn-link collapsed" data-toggle="collapse" data-target={`#collapse${id}`} aria-expanded="false" aria-controls={`collapse${id}`}>
                         {name}
                     </button>
-                </h5>
+                </h4>
                 </div>
 
                 <div id={`collapse${id}`} className="collapse" aria-labelledby={`heading${id}`} data-parent="#accordion">
--- src/layouts/Cities.js
+++ src/layouts/Cities.js
@@ -7,24 +7,23 @@ class Cities extends Component {
     }
     
     componentDidMount(){
-        const API = "https://api.openaq.org/v1/latest?parameter=pm25&order_by=measurements[0].value&sort=desc&country=";
-        const country = this.props.country;
-        fetch(API + country)
+        const API = 'https://api.openaq.org/v1/latest?parameter=pm25&order_by=measurements[0].value&sort=desc&country=';
+        fetch(API + this.props.country)
             .then(response => response.json())
             .catch(error => console.log('Error:', error))
             .then(data => {
                 let cities = data.results.map(city => city.city);
                 cities = cities.filter((v,i) => cities.indexOf(v) === i)
                 cities = cities.slice(0, 10);
-                return this.setState({cities})
+                this.setState({cities})
             });
     }
 
     render() {
-        const CitiesList = [...this.state.cities].map((city, index) => <City name={city} key={index} id={index}/>)
+        const citiesList = [...this.state.cities].map((city, index) => <City name={city} key={index} id={index}/>)
         return (
             <div id="accordion">
-               {CitiesList} 
+               {citiesList} 
             </div>
         );
     }
